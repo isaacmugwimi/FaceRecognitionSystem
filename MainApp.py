6201d2b582909772e32ed4ddc08935036c7dc705
Isaac <mugwimiisaac230@gmail.com>
2024-02-01 11:03:47 +0300

    changes

--- MainApp.py
+++ MainApp.py
@@ -5,12 +5,22 @@ class Main():
     def __init__(self):
         self.root = Tk()
         self.root.geometry("1530x790+0+0")
-        self.root.overrideredirect(True)
+        
         self.root.iconbitmap("images/favicon (2).ico")
-        Label(text="I love coding")
+        
         self.root.mainloop()
         
         
 
+
+
+
+
+
+
+
 if __name__ == '__main__':
+     
+
+
      Main()
